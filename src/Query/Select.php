9f18f5cb58c06f98ba29d6503cc195772249ce9f
veltrup <veltrup@sitepark.com>
2023-12-19 14:13:26 +0100

    refactor: Rename ResourceSearchResult to SearchResult

--- src/Query/Select.php
+++ src/Query/Select.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Atoolo\GraphQL\Search\Query;
 
 use Atoolo\GraphQL\Search\Input\SearchInput;
-use Atoolo\GraphQL\Search\Types\Resource;
-use Atoolo\Search\Dto\Search\Result\ResourceSearchResult;
+use Atoolo\Search\Dto\Search\Result\SearchResult;
 use Atoolo\Search\SelectSearcher;
 use Overblog\GraphQLBundle\Annotation as GQL;
 
@@ -18,11 +17,8 @@ class Select
     ) {
     }
 
-    /**
-     * @return ResourceSearchResult
-     */
-    #[GQL\Query(name: 'search', type: 'ResourceSearchResult!')]
-    public function search(SearchInput $input): ResourceSearchResult
+    #[GQL\Query(name: 'search', type: 'SearchResult!')]
+    public function search(SearchInput $input): SearchResult
     {
         $factory = new SelectQueryFactory();
         $query = $factory->create($input);
