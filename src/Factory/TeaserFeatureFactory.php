0ee1205f5e3d9b9ebc6bbd6a077c31a912dff38c
Felix Becker <167435367+sitepark-becker@users.noreply.github.com>
2025-04-02 15:19:02 +0200

    refactor(teaser):  replaced "actions" with "teaser features" (#81)
    * feat: added teaser features, removed teaser actions
    
    * test: rewrote action link tests to teaser feature tests
    
    * feat: TeaserFeature should be an interface
    
    * feat: remove link from TeaserFeature

--- src/Factory/TeaserFeatureFactory.php
+++ src/Factory/TeaserFeatureFactory.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Atoolo\GraphQL\Search\Factory;
 
-use Atoolo\GraphQL\Search\Types\Link;
+use Atoolo\GraphQL\Search\Types\TeaserFeature;
 use Atoolo\Resource\Resource;
 
-interface ActionLinkFactory
+interface TeaserFeatureFactory
 {
     /**
-     * @return Link[]
+     * @return TeaserFeature[]
      */
     public function create(
         Resource $resource,
