1d4c3ec711184da7bae0eafd3b0e95bed4bcef68
Felix Becker <becker@sitepark.com>
2025-04-03 10:24:14 +0200

    test: added test for TeaserFeature in ResolverMapRegistry

---
+++ test/Types/FakeTeaserFeature.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atoolo\GraphQL\Search\Test\Types;
+
+use Atoolo\GraphQL\Search\Types\TeaserFeature;
+
+class FakeTeaserFeature extends TeaserFeature {}
