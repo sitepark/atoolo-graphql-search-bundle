d53aafda9eccca9c0620ab64b8890fe06189f744
Holger Veltrup <92872893+sitepark-veltrup@users.noreply.github.com>
2026-01-12 13:03:18 +0100

    feat: sameNavigation Support for kicker (#94)

--- src/Query/Context/ContextDispatcher.php
+++ src/Query/Context/ContextDispatcher.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Atoolo\GraphQL\Search\Query\Context;
 
 use Atoolo\GraphQL\Search\Input\SearchContextInput;
+use Atoolo\GraphQL\Search\Resolver\Resource\ResourceResolverContext;
 use Atoolo\Rewrite\Service\UrlRewriteContext;
 
 class ContextDispatcher
 {
     public function __construct(
         private readonly UrlRewriteContext $urlRewriteContext,
+        private readonly ResourceResolverContext $resourceResolverContext,
     ) {}
 
     public function dispatch(SearchContextInput $context): void
@@ -20,11 +22,11 @@ class ContextDispatcher
         }
         if ($context->resourceLocation !== null) {
             $this->urlRewriteContext->setResourceLocation($context->resourceLocation);
+            $this->resourceResolverContext->setResourceLocation($context->resourceLocation);
         }
-        if ($context->options !== null) {
-            if ($context->options->sameNavigation !== null) {
-                $this->urlRewriteContext->setSameNavigation($context->options->sameNavigation);
-            }
+        if (($context->options !== null) && $context->options->sameNavigation !== null) {
+            $this->urlRewriteContext->setSameNavigation($context->options->sameNavigation);
+            $this->resourceResolverContext->setSameNavigation($context->options->sameNavigation);
         }
     }
 }
